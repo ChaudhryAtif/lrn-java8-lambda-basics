ab126199d1f30f2421c81089828a688344e7211e
Atif Chaudhry <achaudh5@slu.edu>
2018-02-16 22:20:45 -0600

    - Demo of how 'this' reference works in lambdas - Rename interface, uncomment code in ClosuresExample

--- java8-lambda-basics/src/com/aic/unit2/ClosuresExample.java
+++ java8-lambda-basics/src/com/aic/unit2/ClosuresExample.java
@@ -5,22 +5,22 @@ public class ClosuresExample {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-//        doProcess(a, new MyProcess() {
-//            @Override
-//            public void process(int i) {
-//                System.out.println(i + b);
-//            }
-//        });
+        doProcess(a, new Process() {
+            @Override
+            public void process(int i) {
+                System.out.println(i + b);
+            }
+        });
 
         doProcess(a, (i) -> System.out.println(i + b));
     }
 
-    public static void doProcess(int i, MyProcess p) {
+    public static void doProcess(int i, Process p) {
         p.process(i);
     }
 
 }
 
-interface MyProcess {
+interface Process {
     void process(int i);
 }
